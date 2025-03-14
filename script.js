91f7a5f13762413f4b5f921a4ede05852888830c
Rana Musa <itsrana88@gmail.com>
2025-03-14 21:59:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('show');
+}
+
+function toggleSearch() {
+    document.querySelector(".search-sidebar").classList.toggle("active");
+}
